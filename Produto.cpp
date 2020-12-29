b0f2c0e9ae9be853b6c2155e97c6aea99c9e9830
jfred24 <58302405+jfred24@users.noreply.github.com>
2020-12-29 01:04:18 +0000

    BuyProduct e alteraçoes

--- Produto.cpp
+++ Produto.cpp
@@ -8,12 +8,14 @@
  * @param qualidade nota qualitativa de má a excelente atribuida ao Produto
  * @param preco preço do Produto
  */
-Produto::Produto(string nome, int numero, tipo_produto tprod, nota_avaliacao qualidade, float preco){
+Produto::Produto(string nome, int numero, tipo_produto tprod, nota_avaliacao qualidade, float preco, int s, string sup){
     this->nome = nome;
     this->numero = abs(numero);
     this->tprod = tprod;
     this->qualidade = qualidade;
     this->preco = preco;
+    this->supplier = sup;
+    this->stock = s;
 }
 
 /**
@@ -27,4 +29,32 @@ void Produto::Info() const{
     cout << "Preco: " << preco << " | ";
     cout << "Qualidade: " << qualidade;
     cout << endl;
+}
+
+bool Produto::operator<(const Produto &p) const {
+    if (getRating() < p.getRating()) {
+        return true;
+    }
+    else if (getRating() > p.getRating()) {
+        return false;
+    }
+    else return getStock() < p.getStock();
+}
+
+BuyProduct::BuyProduct(string nome, int numero, tipo_produto tprod, nota_avaliacao qualidade, float preco, int s, string sup) : Produto(nome, numero, tprod, qualidade, preco, s, sup) {}
+
+int Produto::getStock() const {
+    return stock;
+}
+
+string Produto::getSupplier() const {
+    return supplier;
+}
+
+nota_avaliacao Produto::getRating() const {
+    return qualidade;
+}
+
+priority_queue<Produto> BuyProduct::getCompras() const {
+    return compras;
 }
