183f4d227de34c89e88cf63b0dea53754c964b60
jfred24 <58302405+jfred24@users.noreply.github.com>
2020-12-29 01:02:59 +0000

    class BuyProduct e alteracoes a Product

--- Produto.h
+++ Produto.h
@@ -5,7 +5,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-
+#include <queue>
 
 using namespace std;
 
@@ -19,12 +19,24 @@ public:
     tipo_produto tprod;
     nota_avaliacao qualidade;
     float preco;
-    Produto(string nome, int numero, tipo_produto tprod, nota_avaliacao qualidade, float preco);
+    int stock; //stock no hotel ou na loja? dont know //new
+    string supplier; // new
+    Produto(string nome, int numero, tipo_produto tprod, nota_avaliacao qualidade, float preco, int s, string sup); //foi alterada
     ~Produto() {};
     void Info() const;
     int ID() const {return numero;}
+    int getStock() const;
+    string getSupplier() const;
+    nota_avaliacao getRating() const;
+    bool operator < (const Produto & p) const; //nnc sei se ta certo lol
     bool operator==(const Produto &p2) const{return numero == p2.numero;}
 };
 
-
+class BuyProduct: public Produto {  //n tem de ser derivada mas por agr é
+private:
+    priority_queue<Produto> compras;
+public:
+    BuyProduct(string nome, int numero, tipo_produto tprod, nota_avaliacao qualidade, float preco, int s, string sup);
+    priority_queue<Produto> getCompras() const;
+};
 #endif //HOTEL_PRODUTO_H
