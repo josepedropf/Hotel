73f67eeec2b8cc877c64604edf06cbdacf2dbec7
jfred24 <58302405+jfred24@users.noreply.github.com>
2021-01-04 02:02:09 +0000

    documentaçao

--- Frota.cpp
+++ Frota.cpp
@@ -10,23 +10,35 @@ bool Veiculo::operator<(const Veiculo &v) const {
 bool Veiculo::operator==(const Veiculo &v) const {
     return getMatricula() == v.getMatricula();
 }
-
+/**
+ * @return matricula do veículo
+ */
 matricula Veiculo::getMatricula() const {
     return vmatricula;
 }
-
+/**
+ * @return os kilometros feitos desde o aluguer do veículo
+ */
 double Veiculo::getKms() const {
     return kms;
 }
-
+/**
+ * @return lugares do veiculo
+ */
 int Veiculo::getLugares() const {
     return lugares;
 }
-
+/**
+ * Esta função atualiza os kilometros percorridos pelo veiculo
+ * Caso exceda 5000km chama a função devolveVeículo
+ * @param m kms a acrescentar
+ */
 void Veiculo::updateKms(double kms_feitos) {
     kms += kms_feitos;
 }
-
+/**
+ * Imprime as informações do veículo
+ */
 void Veiculo::Info() const {
     cout << "<VEICULO>" << endl;
     cout << "Matricula: " << vmatricula << " | ";
